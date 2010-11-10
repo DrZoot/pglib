048959c1160989255cc869216816428637cc82f0
unknown <gowepa0@.stmonicas.qld.edu.au>
2010-11-10 16:37:31 +1000

    PGUserManager initial model notes

---
+++ PGUserManager/main.py
@@ -0,0 +1,11 @@
+"""
+main.py
+PGUserManager
+
+Created by Paul Gower on 2010-11-10.
+Copyright 2010 Monotone Software. All rights reserved.
+
+User management API that uses BigTable for storage.
+"""
+from google.appengine.api import users
+
