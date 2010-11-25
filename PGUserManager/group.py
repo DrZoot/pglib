d8c59469d00e84242f252520e41642973f5e4cf0
Paul Gower <p.gower@gmail.com>
2010-11-25 21:44:43 +1000

    Removed unused exception class

--- PGUserManager/group.py
+++ PGUserManager/group.py
@@ -15,17 +15,4 @@ def create_group(name,description=None):
 def delete_group(name,):
   """docstring for delete_group"""
   pass
-  
-
-
-class NameAlreadyUsed(Exception):
-  """
-  --Description--
-  Raised if someone tries to create a group with a name that already exists.
-  """
-  def __init__(self,value):
-    self.value = value
-
-  def __str__(self):
-    return repr(self.value)
     
