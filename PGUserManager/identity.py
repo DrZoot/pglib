df716a55d287fa81eebaf39425b5dc665fd8c425
Paul Gower <p.gower@gmail.com>
2010-11-27 22:38:29 +1000

    REMOVED useless cruft at the top of the file

--- PGUserManager/identity.py
+++ PGUserManager/identity.py
@@ -8,18 +8,6 @@ Functions for manipulating identities
 """
 import models
 import exceptions
-"""
-http://stackoverflow.com/questions/1054868/recursive-delete-in-google-app-engine
-
-You need to implement this manually, by looking up affected records and deleting them at the same time as you delete the parent record. You can simplify this, if you wish, by overriding the .delete() method on your parent class to automatically delete all related records.
-
-For performance reasons, you almost certainly want to use key-only queries (allowing you to get the keys of entities to be deleted without having to fetch and decode the actual entities), and batch deletes. For example:
-
-db.delete(Bottom.all(keys_only=True).filter("daddy =", top).fetch(1000))
-"""
-# =============
-# = Functions =
-# =============
 
 def create_identity(email_address):
   """
