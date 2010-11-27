e1494d2b8189a85cbeac16f4396430b7767bb068
Paul Gower <p.gower@gmail.com>
2010-11-27 23:17:42 +1000

    ADDED import statements for models and exceptions ADDED bind_permission function

--- PGUserManager/permission.py
+++ PGUserManager/permission.py
@@ -6,6 +6,9 @@ Copyright (c) 2010 Monotone Software. All rights reserved.
 
 Functions for manipulating Permissions
 """
+import models
+import exceptions
+
 def create_permission(name,description=None):
   """Create a new permission or raise a NameAlreadyUsed exception if a permission already exists with that name"""
   uniqueness_query = models.Permission.all(keys_only=True).filter('name',name)
@@ -22,3 +25,22 @@ def get_permission(name):
 def permission_query(*args,**kwargs):
   """Return a query for permissions"""
   return models.Permission.all(*args,**kwargs)
+  
+def bind_permission(permission,subject):
+  """Create a permission binding between the given permission and the subject (group/identity)"""
+  if not isinstance(permission,models.Permission):
+    raise Exception("Must pass permission object, not key.")
+  else:
+    permission_name = permission.name
+  if not isinstance(subject,models.Identity) or not isinstance(subject,models.Group):
+    raise Exception("Must pass either an Identity object or a Group object, not keys.")
+  else:
+    subject_name = subject.key().name
+  permission_binding_name = permission_name + "_" + subject_name
+  if models.PermissionBinding.get_by_key_name(permission_binding_name):
+    raise Exception("PermissionBinding already exists")
+  else:
+    key = models.PermissionBinding(key_name=permission_binding_name,permission=permission,subject=subject).put()
+    return models.PermissionBinding.get(key)
+      
+  
