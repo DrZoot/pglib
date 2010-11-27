a9d8a294ddff049aaa54f36bacd6df26ba710598
Paul Gower <p.gower@gmail.com>
2010-11-27 22:21:15 +1000

    ADDED create_group function REMOVED delete_group function ADDED group_query function

--- PGUserManager/group.py
+++ PGUserManager/group.py
@@ -6,13 +6,24 @@ Copyright (c) 2010 Monotone Software. All rights reserved.
 
 Functions for manipulating groups
 """
+import models
+import exceptions
+
 def create_group(name,description=None):
-  """docstring for create_group"""
-  if not description:
-    description = ""
-  pass
+  """create a group with the given name, if a group with the same name exists raise """
+  uniqueness_query = models.Group.all(keys_only=True).filter('name',name)
+  if uniqueness_query.get():
+    raise exceptions.NameAlreadyUsed(name)
+  else:
+    key = models.Group(key_name=name,name=name,description=description).put()
+    return models.Group.get(key)
   
-def delete_group(name,):
-  """docstring for delete_group"""
-  pass
+  
+def get_group(name):
+  """return the group with the given name or none"""
+  return models.Group.all().filter('name',name).get()
+  
+def group_query(*args,**kwargs):
+  """return a query for groups"""
+  return models.Group.all(*args,**kwargs)
     
