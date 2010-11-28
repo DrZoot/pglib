133fe686d334b1ee439eb47368cd17073db47bb7
Paul Gower <p.gower@gmail.com>
2010-11-28 21:34:18 +1000

    MODIFIED verify_arg, was not creating a valid tuple from the args

--- PGUserManager/utils.py
+++ PGUserManager/utils.py
@@ -24,7 +24,7 @@ def verify_arg(arg,*args):
   """given an input argument and a list of models make sure that the input is one of the model types"""
   if isinstance(arg,db.Key):
     arg = db.get(arg)
-  if isinstance(arg,tuple(*args)):
+  if isinstance(arg,tuple(args)):
     return arg
   else:
     raise TypeError('arg must be one of:' + repr(*args))
