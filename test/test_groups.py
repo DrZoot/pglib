d11687fa9cb1f6b738063596e54f482e09a15208
Paul Gower <p.gower@gmail.com>
2010-11-28 21:46:32 +1000

    ADDED testCreateGroupQuery, testDeleteGroup, testAddIdentitiesToGroup, testAddIdentitiesToMultipleGroups, testAddIdentitiesToGroupsKeysOnly

--- test/test_groups.py
+++ test/test_groups.py
@@ -26,5 +26,47 @@ class GroupTestsOne(unittest.TestCase):
     
   def testRetrieveGroupByName(self):
     # retrieve a group from the datstore by name
+    group.create_group('Group1')
+    g = group.get_group('Group1')
+    self.assert_(isinstance(g,models.Group), 'Must return an object of type Group')
+    self.assert_(g.name == 'Group1', 'Returned group must be the one created')
+    
+  def testCreateGroupQuery(self):
+    # create a custom query for groups
+    self.assert_(isinstance(group.group_query(),db.Query), 'group.group_query must return db.Query type')
+    
+  def testDeleteGroup(self):
+    # delete a group
+    g = group.create_group('Group1')
+    g.delete()
+    self.assert_(group.get_group('Group1') == None, 'Deleted group should not exist on the datastore')
+    
+  def testAddIdentitiesToGroup(self):
+    # add identities to a group
+    g = group.create_group('Group1')
+    identities = [ident for ident in identity.identity_query()]
+    for ident in identities:
+      group.add_member(g,ident)
+    self.assertEqual(10, g.identity_bindings.count(), 'Number of membership bindings must equal the number of identities bound (10)')
+    
+  def testAddIdentitiesToMultipleGroups(self):
+    # add identities to a group
+    g = group.create_group('Group1')
+    g2 = group.create_group('Group2')
+    identities = [ident for ident in identity.identity_query()]
+    for ident in identities:
+      group.add_member(g,ident)
+      group.add_member(g2,ident)
+    self.assertEqual(10, g.identity_bindings.count(), 'Number of membership bindings must equal the number of identities bound (10)')
+    self.assertEqual(10, g2.identity_bindings.count(), 'Number of membership bindings must equal the number of identities bound (10)')
+    
+  def testAddIdentitiesToGroupKeysOnly(self):
+    # add identities to a group, use keys for the group and identities
+    g_key = group.create_group('Group1').key()
+    i_keys = [ik for ik in identity.identity_query(keys_only=True)]
+    for ik in i_keys:
+      group.add_member(g_key,ik)
+    self.assertEqual(10, group.get_group('Group1').identity_bindings.count(), 'Number of membership bindings must equal the number of identities bound (10)')
+    
     
 
