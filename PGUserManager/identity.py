2966b8c6e2be5021e8afa3ff229f9b1dbb325c96
Paul Gower <p.gower@gmail.com>
2010-12-02 14:33:45 +1000

    MODIFED get_identity to only return an identity if active=True ADDED optional param to create_identity to specify wether an identity is active or not MODIFIED identity_for_current_user to use get_identity for identity retrieval and therefore return None if the current identity is inactive.

--- PGUserManager/identity.py
+++ PGUserManager/identity.py
@@ -10,7 +10,7 @@ import models
 import exceptions
 from google.appengine.api import users
 
-def create_identity(email_address):
+def create_identity(email_address,active=True):
   """
   Creates a new identity in the datstore and returns the identity object or if the email has already been used raises an DuplicateValue exception with the 
   """
@@ -18,14 +18,18 @@ def create_identity(email_address):
   if models.Identity.get_by_key_name(key_name):
     raise exceptions.DuplicateValue(email_address)
   else:
-    key = models.Identity(key_name=key_name,email=email_address).put()
+    key = models.Identity(key_name=key_name,email=email_address,active=active).put()
     return models.Identity.get(key)
 
 def get_identity(email_address):
   """
   Given an email address try to return a datastore object for it using the email_address as a key_name
   """
-  return models.Identity.get_by_key_name(email_address.lower())
+  i = models.Identity.get_by_key_name(email_address.lower())
+  if i != None and i.active:
+    return i
+  else:
+    return None
   
 def identity_query(*args,**kwargs):
   """
@@ -36,4 +40,4 @@ def identity_query(*args,**kwargs):
 def identity_for_current_user():
   """Return the identity for the currently logged on user or None"""
   key_name = users.get_current_user().email().lower()
-  return models.Identity.get_by_key_name(key_name)
+  return get_identity(key_name)
