010b398586e14625101fd1b5942cc6d79bf49b26
Paul Gower <p.gower@gmail.com>
2010-12-07 09:00:14 +1000

    ADDED the **kwargs argument to the end of the create_identity function so that any number of params can be passed and they will be added to the identity ADDED include_inactive param to the get_identity method

--- PGUserManager/identity.py
+++ PGUserManager/identity.py
@@ -10,26 +10,31 @@ import models
 import exceptions
 from google.appengine.api import users
 
-def create_identity(email_address,active=True):
+def create_identity(email_address,active=True,**kwargs):
   """
-  Creates a new identity in the datstore and returns the identity object or if the email has already been used raises an DuplicateValue exception with the 
+  Creates a new identity in the datstore and returns the identity object or if the email has already been used raises an DuplicateValue exception 
   """
   key_name = email_address.lower()
   if models.Identity.get_by_key_name(key_name):
     raise exceptions.DuplicateValue(email_address)
   else:
-    key = models.Identity(key_name=key_name,email=email_address,active=active).put()
+    key = models.Identity(key_name=key_name,email=email_address,active=active,**kwargs).put()
     return models.Identity.get(key)
 
-def get_identity(email_address):
+def get_identity(email_address,include_inactive=False):
   """
   Given an email address try to return a datastore object for it using the email_address as a key_name
   """
   i = models.Identity.get_by_key_name(email_address.lower())
-  if i != None and i.active:
+  if i == None:
+    return None
+  else if i.active:
     return i
   else:
-    return None
+    if include_inactive:
+      return i
+    else:
+      return None
   
 def identity_query(*args,**kwargs):
   """
