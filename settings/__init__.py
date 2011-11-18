ce14ed2cb48633a12ad645282f4960eea816a8e4
DrZoot <p.gower@gmail.com>
2011-11-18 11:34:27 +1000

    Stil cleaning up repo, really need to remember to delete/rename the git way

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,1 +1,8 @@
-__all__ = ['settings']
+import functions
+import exceptions
+
+__all__ = ['functions']
+
+get = functions.get
+set = functions.set
+load = functions.load
--- settings/models.py
+++ settings/models.py
@@ -1,36 +1,11 @@
 from google.appengine.ext import db
-from google.appengine.api import memcache
 
-class Setting (db.polymodel)
+class Setting (db.Expando):
   """
-  Base class for a key-value settings entry
+  Holds a setting
   """
-  key = db.StringProperty(required=True)
+  index = db.StringProperty(required=True)
   owner = db.UserProperty(auto_current_user_add=True)
-  is_global = db.BoolProperty(required=True, default=False)
-  
-class StringSetting (Setting)
-  """
-  Holds a string value
-  """
-  value = db.StringProperty(required=True)
-  
-class IntSetting (Setting)
-  """
-  Holds an integer value
-  """
-  value = db.IntegerProperty(required=True)
-  
-class BoolSetting(Setting)
-  """
-  Holds a bool value
-  """
-  value = db.BoolProperty(required=True)
-  
-class FloatSetting(Setting)
-  """
-  Holds a float value
-  """
-  value = db.FloatProperty(required=True)  
- 
+  is_global = db.BooleanProperty(required=True, default=False)
+  # value is the expando property here
   
